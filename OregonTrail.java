ae4c3bd65b8151dad013bc6797c0625d85ce28f9
at and sz <at  anjatimofeyeva@gmail.com sz  github@zaske.com>
2017-06-27 13:23:55 -0700

    fix for survival bug

--- OregonTrail.java
+++ OregonTrail.java
@@ -7,19 +7,19 @@ public class OregonTrail {
   public static void main(String[] args){
     //create a console object to get user feedback
     Console myConsole = System.console();
-    Boolean madeIt = false; // did they make it to the end
+    Boolean died = false; // did they make it to the end
 
     //create our threats
     //create a arraylist
     List<Threat> oregonTrailThreats = new ArrayList<Threat>();
     //create each threat
-    Threat newThreat1 = new Threat("A snake lunges at you and...",.1);
-    Threat newThreat2 = new Threat("You get smallpox and...",.1);
+    Threat newThreat1 = new Threat("A snake lunges at you and...",.07);
+    Threat newThreat2 = new Threat("You get smallpox and...",.06);
     Threat newThreat3 = new Threat("A get attacked by a bear and...",.1);
-    Threat newThreat4 = new Threat("A get dysentry and...",.1);
-    Threat newThreat5 = new Threat("You're running low on water and...",.15);
+    Threat newThreat4 = new Threat("A get dysentry and...",.05);
+    Threat newThreat5 = new Threat("You're running low on water and...",.09);
     Threat newThreat6 = new Threat("You get struck by lightning and...",.5);
-    Threat newThreat7 = new Threat("A accidently shoot yourself and...",.3);
+    Threat newThreat7 = new Threat("A accidently shoot yourself and...",.25);
 
     //add threats to ArrayList
     oregonTrailThreats.add(newThreat1);
@@ -31,7 +31,7 @@ public class OregonTrail {
     oregonTrailThreats.add(newThreat7);
 
     //run though 10 threats
-    for(int i=1; i<10; i++){
+    for(int i=1; i<=10; i++){
 
       //walk a ways down the trail
       System.out.println("You traveled " + i + "/10th of the Oregon Trail");
@@ -46,25 +46,27 @@ public class OregonTrail {
       String trailChoice = myConsole.readLine();
 
       //logic for if they live or die
-      int max = 1;
+      int max = 100;
       int min = 0;
-      int survive = rand.nextInt((max - min) + 1) + min;
-
+      Integer tempSurvive = rand.nextInt((max - min) + 1) + min;
+      System.out.println(tempSurvive);
+      double survive = tempSurvive.doubleValue()/max;
+      System.out.println(survive);
       if (survive > oregonTrailThreats.get(randThreat).deathPercent){
         //you survive
         System.out.println("...YOU SURVIVED!");
       } else {
         System.out.println("...YOU DIED!");
+        died=true;
         break;
       }
     } // end of for loop
 
-    if (madeIt) {
-      System.out.println("Congratulations, you're a hardy survivor of the Oregon Trail, you leave to a ripe old age!");
+    if (died) {
+      System.out.println("Sorry, but your corpse was buried along the way, you failed...loser.");
     }
     else {
-      System.out.println("Sorry, but your corpse was buried along the way, you failed...loser.");
-
+      System.out.println("Congratulations, you're a hardy survivor of the Oregon Trail, you leave to a ripe old age!");
     }
 
     //test it
